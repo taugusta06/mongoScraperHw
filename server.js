710d8c35f4b4075e360e629180c40cca414372df
Ty Augusta <taugusta06@gmail.com>
2018-08-27 08:03:24 -0700

    update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // // Set mongoose to leverage built in JavaScript ES6 Promises
 // // Connect to the Mongo DB
-// mongoose.Promise = Promise;
-// mongoose.connect(MONGODB_URI);
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 
 var express = require('express');
